69f31c088704c2662f7695f229a253ff60495d7a
Tom Hackshaw <gh@tomhackshaw.com>
2025-01-04 23:07:41 +1300

    Comment out unused imports on landing page

--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,8 @@ import { SiteHeader } from "@/components/(landing-page)/site-header";
 import { HeroSection } from "@/components/(landing-page)/(sections)/hero-section";
 import { FeaturesSection } from "@/components/(landing-page)/(sections)/features-section";
 import { GetStartedSection } from "@/components/(landing-page)/(sections)/get-started-section";
-import { TemplatesSection } from "@/components/(landing-page)/(sections)/templates-section";
-import { ToolsSection } from "@/components/(landing-page)/(sections)/tools-section";
+/* import { TemplatesSection } from "@/components/(landing-page)/(sections)/templates-section";
+import { ToolsSection } from "@/components/(landing-page)/(sections)/tools-section"; */
 import { SiteFooter } from "@/components/(landing-page)/site-footer";
 
 export default function Home() {
