1930198b589b37eb3d04ef5f6ee41c266be0a140
Tom Hackshaw <gh@tomhackshaw.com>
2025-01-04 23:17:30 +1300

    Better scaling logos and images

--- components/(landing-page)/(sections)/hero-section.tsx
+++ components/(landing-page)/(sections)/hero-section.tsx
@@ -8,6 +8,17 @@ export function HeroSection() {
       <div className="grid gap-6 lg:grid-cols-[1fr_500px] lg:gap-12 xl:grid-cols-[1fr_550px]">
         <div className="flex flex-col justify-center space-y-8">
           <div className="space-y-6">
+            <div className="flex items-center justify-center lg:hidden">
+              <div className="relative w-full h-[400px]">
+                <Image
+                  src="/hero.svg"
+                  alt="Illustration of a person using Tabula"
+                  className="object-contain"
+                  fill
+                  priority
+                />
+              </div>
+            </div>
             <h1 className="text-4xl font-bold tracking-tighter sm:text-5xl xl:text-6xl/none">
               All-in-one workspace for art teachers
             </h1>
@@ -35,36 +46,36 @@ export function HeroSection() {
               <Image
                 src="/tgs.svg"
                 alt="Takapuna Grammar School"
-                className="h-8 w-auto"
+                className="md:h-8 h-7 w-auto"
                 width={100}
                 height={32}
               />
               <Image
                 src="/liston.svg"
                 alt="Liston College"
-                className="h-8 w-auto"
+                className="md:h-8 h-7 w-auto"
                 width={100}
                 height={32}
               />
               <Image
                 src="/westlake.svg"
                 alt="Westlake Boys High School"
-                className="h-8 w-auto"
+                className="md:h-8 h-7 w-auto"
                 width={100}
                 height={32}
               />
               <Image
                 src="/lynfield.svg"
                 alt="Lynfield College"
-                className="h-8 w-auto"
+                className="md:h-8 h-7 w-auto"
                 width={100}
                 height={32}
               />
             </div>
           </div>
         </div>
-        <div className="flex items-center justify-center">
-          <div className="relative w-full h-[400px] lg:h-[600px]">
+        <div className="hidden lg:flex items-center justify-center">
+          <div className="relative w-full h-[600px]">
             <Image
               src="/hero.svg"
               alt="Illustration of a person using Tabula"
