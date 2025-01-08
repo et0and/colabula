b2e7074e29e0f3568a2d080afb2c6bf2dc2c78ba
Tom Hackshaw <gh@tomhackshaw.com>
2025-01-08 23:04:25 +1300

    Roadmap and status page scaffold

--- components/(landing-page)/site-footer.tsx
+++ components/(landing-page)/site-footer.tsx
@@ -9,11 +9,15 @@ export function SiteFooter() {
             <p className="text-sm text-gray-500">
               © {new Date().getFullYear()} Cold Sundays, Ltd.
             </p>
+
             <div className="flex gap-4 md:gap-8 mt-4 md:mt-0">
-              <Link href="/about" className="link text-sm">
+              <Link href="/about" className="link text-sm hidden md:block">
                 About us
               </Link>
-              <Link href="https://status.aratuku.com" className="link text-sm">
+              <Link
+                href="https://status.aratuku.com"
+                className="link text-sm hidden md:block"
+              >
                 Status
               </Link>
               <Link
@@ -28,6 +32,13 @@ export function SiteFooter() {
               <Link href="/tos" className="link text-sm">
                 Terms of Service{" "}
               </Link>{" "}
+              <Link
+                href="https://lindie.app/share/e7e2aa1f4c23f3dd5d3895da22714ef85c0aa03a"
+                target="_blank"
+                className="link text-sm"
+              >
+                Roadmap
+              </Link>
             </div>
           </div>
         </div>
