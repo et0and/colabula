1ee5ae700a4e791f5d46b72c2600fc0e26373fa6
Tom Hackshaw <gh@tomhackshaw.com>
2025-01-13 19:56:26 +1300

    Init

---
+++ components/(landing-page)/(sections)/comments-ui.tsx
@@ -0,0 +1,23 @@
+import { TiltedScroll } from "@/components/ui/tilted-scroll";
+
+export function CommentsUi() {
+  const customItems = [
+    { id: "1", text: "Great work!" },
+    {
+      id: "2",
+      text: "I think this is tracking as an Achieved High, but if they were to ...",
+    },
+    { id: "3", text: "What artist models did they look at for this?" },
+    { id: "4", text: "The last work finishes the board well. Nice work!" },
+    {
+      id: "5",
+      text: "This is nearly there. I would highly recommend they look at Daido Moriyama's work...",
+    },
+  ];
+
+  return (
+    <div className="space-y-8">
+      <TiltedScroll items={customItems} className="mt-8" />
+    </div>
+  );
+}
--- components/(landing-page)/(sections)/features-section.tsx
+++ components/(landing-page)/(sections)/features-section.tsx
@@ -1,17 +1,22 @@
 import { Brain, GitBranch, Zap } from "lucide-react";
 import { featuresContent } from "@/lib/strings";
-
+import { CommentsUi } from "./comments-ui";
 export function FeaturesSection() {
   return (
     <section className="container px-4 md:px-6 py-12">
       <div className="max-w-[1200px] mx-auto">
-        <div className="space-y-6 mb-12">
-          <h2 className="text-3xl font-bold tracking-tighter sm:text-4xl md:text-5xl">
-            {featuresContent.title}{" "}
-          </h2>
-          <p className="text-gray-500 md:text-xl">
-            {featuresContent.description}
-          </p>
+        <div className="flex gap-8 mb-12">
+          <div className="space-y-6">
+            <h2 className="text-3xl font-bold tracking-tighter sm:text-4xl md:text-5xl">
+              {featuresContent.title}{" "}
+            </h2>
+            <p className="text-gray-500 md:text-xl">
+              {featuresContent.description}
+            </p>
+          </div>
+          <div className="hidden md:block">
+            <CommentsUi />
+          </div>
         </div>
 
         <div className="grid md:grid-cols-3 gap-8 mb-12">
@@ -31,7 +36,9 @@ export function FeaturesSection() {
           </div>
           <div className="space-y-3">
             <Zap className="h-8 w-8" />
-            <h3 className="font-semibold">{featuresContent.featureThreeTitle}</h3>
+            <h3 className="font-semibold">
+              {featuresContent.featureThreeTitle}
+            </h3>
             <p className="text-gray-500">
               {featuresContent.featureThreeDescription}
             </p>
