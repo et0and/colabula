b2cbc74eb3f62114db867ca7f092a2c9ac440fa1
Tom Hackshaw <gh@tomhackshaw.com>
2025-01-13 19:21:33 +1300

    Notice banner for signups

---
+++ app/(auth)/sign-up/_components/notice.tsx
@@ -0,0 +1,25 @@
+"use client";
+
+import { useState } from "react";
+import { AlertCircle } from "lucide-react";
+import { Banner } from "@/components/ui/banner";
+
+export function NoticeBanner() {
+  const [show, setShow] = useState(true);
+
+  return (
+    <Banner
+      show={show}
+      onHide={() => setShow(false)}
+      icon={<AlertCircle className="m-px h-4 w-4 text-green-800" />}
+      title={
+        <>
+          Only schools participating in the private beta can currently sign up
+          for an account. If you would like to give Aratuku a go, please contact
+          us.
+        </>
+      }
+      learnMoreUrl="https://dub.co/help/article/free-dot-link-domain"
+    />
+  );
+}
